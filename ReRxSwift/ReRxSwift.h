cee5de74662df78ce02f0eb2a2beec03ba5dae5f
Stefan van den Oord <soord@mac.com>
2017-08-16 16:55:15 +0200

    Initial Xcode project

---
+++ ReRxSwift/ReRxSwift.h
@@ -0,0 +1,19 @@
+//
+//  ReRxSwift.h
+//  ReRxSwift
+//
+//  Created by Stefan on 16/08/2017.
+//  Copyright © 2017 Stefan van den Oord. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReRxSwift.
+FOUNDATION_EXPORT double ReRxSwiftVersionNumber;
+
+//! Project version string for ReRxSwift.
+FOUNDATION_EXPORT const unsigned char ReRxSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReRxSwift/PublicHeader.h>
+
+

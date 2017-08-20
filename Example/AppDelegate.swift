34ddf1c0b05b19dc7a6b81ee4401abd8c8e13de4
Stefan van den Oord <soord@mac.com>
2017-08-20 22:16:50 +0200

    Added target for sample app

---
+++ Example/AppDelegate.swift
@@ -0,0 +1,9 @@
+//  Copyright © 2017 Stefan van den Oord. All rights reserved.
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
+
---
+++ Example/ViewController.swift
@@ -0,0 +1,6 @@
+//  Copyright © 2017 Stefan van den Oord. All rights reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+}

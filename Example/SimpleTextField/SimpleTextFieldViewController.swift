d8a89f22f43d2028302534a11adba74603fcf5b0
Stefan van den Oord <soord@mac.com>
2017-08-22 21:56:28 +0200

    Added ReRxSwift mechanics to the SimpleTextFieldViewController

--- Example/SimpleTextField/SimpleTextFieldViewController.swift
+++ Example/SimpleTextField/SimpleTextFieldViewController.swift
@@ -1,7 +1,56 @@
 //  Copyright © 2017 Stefan van den Oord. All rights reserved.
 
 import UIKit
+import ReSwift
+import RxSwift
 import ReRxSwift
 
 class SimpleTextFieldViewController: UIViewController {
+    let connection = Connection(
+        store: store,
+        mapStateToProps: mapStateToProps,
+        mapDispatchToActions: mapDispatchToActions
+    )
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        connection.connect()
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        connection.disconnect()
+    }
+
+    override func viewDidLoad() {
+        connection.bind(\Props.text, to: textField.rx.text)
+    }
+    
+    @IBAction func editingChanged(_ sender: UITextField) {
+        actions.updatedText(sender.text ?? "")
+    }
+    
+    @IBOutlet weak var textField: UITextField!
 }
+
+extension SimpleTextFieldViewController: Connectable {
+    struct Props {
+        let text: String
+    }
+    struct Actions {
+        let updatedText: (String) -> Void
+    }
+}
+
+private let mapStateToProps = { (appState: AppState) in
+    return SimpleTextFieldViewController.Props(
+        text: appState.simpleTextField.content
+    )
+}
+
+private let mapDispatchToActions = { (dispatch: @escaping DispatchFunction) in
+    return SimpleTextFieldViewController.Actions(
+        updatedText: { newText in dispatch(SetContent(newContent: newText)) }
+    )
+}
+

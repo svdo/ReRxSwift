b5f29f2ab5634e5c3f5013b49bdfd5c02e6af8d1
Stefan van den Oord <soord@mac.com>
2017-08-24 12:31:54 +0200

    Changed the view controller tests so that they are the same, for consistency and to facilitate learning from them

--- ExampleTests/SimpleTextField/SimpleTextFieldViewControllerSpec.swift
+++ ExampleTests/SimpleTextField/SimpleTextFieldViewControllerSpec.swift
@@ -10,6 +10,9 @@ class SimpleTextFieldViewControllerSpec: QuickSpec {
         var simpleController: SimpleTextFieldViewController!
 
         beforeEach {
+            store.state = initialAppState
+            // TODO: create a way to inject the store, because also subscriptions should be reset
+
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
             let controller = storyboard.instantiateViewController(withIdentifier: "SimpleTextField")
             guard let simple = controller as? SimpleTextFieldViewController else {
@@ -20,8 +23,25 @@ class SimpleTextFieldViewControllerSpec: QuickSpec {
             expect(simpleController.view).toNot(beNil())
         }
 
-        it("takes text field text from props") {
+        it("has a connection") {
+            expect(simpleController.connection).toNot(beNil())
+        }
+
+        it("has props and actions") {
+            expect(simpleController.props).toNot(beNil())
+            expect(simpleController.actions).toNot(beNil())
+        }
+
+        it("uses props text as text field value") {
             simpleController.props = SimpleTextFieldViewController.Props(text: "some text")
+            expect(simpleController.textField.text) == "some text"
+        }
+
+        it("maps value from state to props") {
+            let state = AppState(
+                simpleTextField: SimpleTextFieldState(content: "some text"),
+                steppingUp: initialSteppingUpState)
+            simpleController.connection.newState(state: state)
             expect(simpleController.textField.text) == "some text"
         }
 
@@ -32,6 +52,34 @@ class SimpleTextFieldViewControllerSpec: QuickSpec {
             )
             simpleController.editingChanged(simpleController.textField)
             expect(actionTriggered).to(beTrue())
+        }
+
+        context("when viewWillAppear has been called") {
+            beforeEach {
+                simpleController.viewWillAppear(false)
+            }
+
+            it("is connected") {
+                let state = AppState(
+                    simpleTextField: SimpleTextFieldState(content: "test content"),
+                    steppingUp: initialSteppingUpState)
+                store.state = state
+                expect(simpleController.textField.text) == "test content"
+            }
+
+            context("when viewDidDisappear has been called") {
+                beforeEach {
+                    simpleController.viewDidDisappear(false)
+                }
+
+                it("is no longer connected") {
+                    let state = AppState(
+                        simpleTextField: SimpleTextFieldState(content: "test content"),
+                        steppingUp: initialSteppingUpState)
+                    store.state = state
+                    expect(simpleController.textField.text) == initialSimpleTextFieldState.content
+                }
+            }
         }
     }
 }

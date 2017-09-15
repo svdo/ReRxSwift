96478975452390b8849fe5ddfba7e2a8da1f8f92
Stefan van den Oord <soord@mac.com>
2017-09-15 20:16:45 +0200

    Added collection view controller to show that it also works. Didn't add any tests, because they are identical to those of the table view controller.

---
+++ Example/TableAndCollection/CollectionViewController.swift
@@ -0,0 +1,68 @@
+//  Copyright © 2017 Stefan van den Oord. All rights reserved.
+
+import UIKit
+import ReSwift
+import RxSwift
+import RxDataSources
+import ReRxSwift
+
+private let mapStateToProps = { (appState: AppState) in
+    return CollectionViewController.Props(
+        categories: appState.tableAndCollection.categories
+    )
+}
+
+private let mapDispatchToActions = { (dispatch: @escaping DispatchFunction) in
+    return CollectionViewController.Actions(reverse: { dispatch(ReverseShops()) })
+}
+
+extension CollectionViewController: Connectable {
+    struct Props {
+        let categories: [ShopCategory]
+    }
+    struct Actions {
+        let reverse: () -> ()
+    }
+}
+
+class CollectionViewController: UICollectionViewController {
+    @IBOutlet var reverseButton: UIBarButtonItem!
+
+    let connection = Connection(store: store,
+                                mapStateToProps: mapStateToProps,
+                                mapDispatchToActions: mapDispatchToActions)
+    var dataSource: RxCollectionViewSectionedAnimatedDataSource<ShopCategory>!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.rightBarButtonItem = self.reverseButton
+        self.collectionView?.dataSource = nil
+        dataSource = RxCollectionViewSectionedAnimatedDataSource<ShopCategory>()
+        dataSource.configureCell = { (dataSource, collectionView, indexPath, item) in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "NormalCell", for: indexPath)
+            (cell.viewWithTag(1) as? UILabel)?.text = item.name
+            (cell.viewWithTag(2) as? UILabel)?.text = String(item.rating)
+            return cell
+        }
+        dataSource.supplementaryViewFactory = { (dataSource, collectionView, kind, indexPath) in
+            let view = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "HeaderView", for: indexPath)
+            (view.viewWithTag(1) as? UILabel)?.text = self.props.categories[indexPath.section].title
+            return view
+        }
+        self.connection.bind(\Props.categories, to: collectionView!.rx.items(dataSource: dataSource))
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        connection.connect()
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        connection.disconnect()
+    }
+
+    @IBAction func reverseTapped(_ sender: UIBarButtonItem) {
+        actions.reverse()
+    }
+}
